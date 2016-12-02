da60a72ca473eaff21e77147020566e51af2dcac
ajaykumar-bollepalli <ajay.bollepalli@gmail.com>
2016-12-02 22:29:10 +0530

    created addContent file

---
+++ addContent.php
@@ -0,0 +1,14 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>Add content to study.rougebird.in</title>
+	</head>
+	<body>
+		<form method="post">
+			<label>Content Title:</label>
+			<input type="Text" name="c_title">
+			<label>Content Description:</label>
+			<textarea name="c_desc"></textarea>
+		</form>
+	</body>
+</html>
